a74875437460608aff14feb17e0c57c423cda9fe
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-22 15:27:38 -0300

    implementa campos de form de login e rota

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div>Hello, TrybeWallet!</div>;
+  return (
+    <>
+      <div>Hello, TrybeWallet!</div>
+      <Switch>
+        <Route path="/" component={ Login } />
+      </Switch>
+    </>
+  );
 }
 
 export default connect()(App);
