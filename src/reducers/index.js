bb5dbb13382205fff21ed654c3e8acf2f8616f9e
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-22 17:43:56 -0300

    muda nomes de importações

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import userReducer from './user';
-// import user from './user';
-import walletReducer from './wallet';
-// import wallet from './wallet';
+import user from './user';
+import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-const reducer = combineReducers({ user: userReducer, wallet: walletReducer });
+const reducer = combineReducers({ user, wallet });
 
 export default reducer;
