3a1b39918c51ed973c9ad871baf6ecf1762b0d2f
Pedro Franca <pedrodepaulafranca@gmail.com>
2022-06-22 16:04:32 -0300

    inclui lógica de validação de senha

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,14 +2,44 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 class Login extends React.Component {
+  state = {
+    // email: '',
+    password: '',
+    disabled: true,
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => {
+      const { password } = this.state;
+      const MIN_PASSWORD_LENGTH = 6;
+      if (password.length >= MIN_PASSWORD_LENGTH) {
+        return this.setState({ disabled: false });
+      }
+      return this.setState({ disabled: true });
+    });
+  }
+
   render() {
+    const { disabled } = this.state;
     return (
       <>
         <div>Login</div>
         <form>
-          <input type="email" data-testid="email-input" />
-          <input type="password" data-testid="password-input" />
-          <button type="button">Entrar</button>
+          <input
+            type="email"
+            name="email"
+            data-testid="email-input"
+            // onChange={ this.handleChange }
+          />
+          <input
+            type="password"
+            name="password"
+            data-testid="password-input"
+            // minLength={ 6 }
+            onChange={ this.handleChange }
+          />
+          <button type="button" disabled={ disabled }>Entrar</button>
         </form>
       </>
     );
